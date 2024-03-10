db917acf733b3f2a3ce7b6c6acce2c7ee257b481
anish-lakkapragada <anish.lakkapragada@gmail.com>
2024-03-09 23:11:59 -0800

    first chart.js section.

--- src/About.tsx
+++ src/About.tsx
@@ -2,6 +2,67 @@ import Officer from "./Officer"
 import grants from './assets/about-grants.png'
 import contributions from "./assets/contributions.png"
 
+import { onMount } from 'solid-js'
+import { Chart, Title, Tooltip, Legend, Colors } from 'chart.js'
+import { Pie } from 'solid-chartjs'
+
+onMount(() => {
+  Chart.register(Title, Tooltip, Legend, Colors)
+})
+
+const chartData = {
+  labels: ['January', 'February', 'March', 'April', 'May'],
+  datasets: [
+      {
+          label: 'Sales',
+          data: [50, 60, 70, 80, 90],
+      },
+  ],
+}
+const chartOptions = {
+  responsive: true,
+  maintainAspectRatio: false,
+  plugins: {
+    title: {
+        display: true,
+        text: 'Grant Approved By Department (All Years)', 
+        font: {
+          size: 20
+        }
+    }, 
+    legend: {
+      position: "bottom"
+    }, 
+    labels: {
+      font: {
+        size: 20
+      }
+    }
+  }
+}
+
+const pieDataGrants = {
+  labels: [
+    'School',
+    'Art',
+    'Business', 
+    'Drama', 
+    'English', 
+    'Mathematics/Computer Science',
+    'Music', 
+    'Physical Education', 
+    'Science', 
+    'Social Studies', 
+    'World Languages',
+    'Special Education', 
+    'Guidance', 
+    'Library'
+  ],
+  datasets: [{
+    data: [32, 4, 1, 1, 7, 0, 10, 5, 5, 21, 3, 3, 1, 2, 5]
+  }]
+};
+
 function About() {
   return <div> 
 
@@ -11,7 +72,8 @@ function About() {
           <h1 class="text-6xl font-extrabold text-center"> Our Mission </h1> 
           <p class="text-2xl font-light text-center mx-20"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sollicitudin enim in dapibus sollicitudin. Sed euismod semper convallis. Mauris vel laoreet tellus. </p> 
         </div> 
-        <img src={grants} class="mb-2 h-96 mx-20"/>
+        {/* <img src={grants} class="mb-2 h-96 mx-20"/> */}
+        <div class="mt-12 md:w-[50%] sm:flex sm:flex-shrink"> <Pie data={pieDataGrants} options={chartOptions} /> </div> 
     </div> 
 
     <div class="w-full gap-4 flex flex-row sm:flex-col"> 
