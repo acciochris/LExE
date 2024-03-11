cd1f0169a902e275f0d321aa1051ea1e23969a9a
Chris Liu <acciochris@outlook.com>
2024-03-11 10:40:35 -0700

    Adjust styles for fundraising section

--- src/Home.tsx
+++ src/Home.tsx
@@ -37,14 +37,14 @@ function Home() {
       
       <div class="grid grid-cols-2 gap-12 sm:grid-cols-1 my-8"> 
         <div>
-          <h1 class="text-4xl font-extrabold text-center -mt-8 drop-shadow-2xl">Fundraising Goals</h1>
-          <p class="text-lg text-left mx-4 mt-4">
+          <h1 class="text-4xl text-center font-extrabold -mt-8 drop-shadow-lg">Fundraising Goals</h1>
+          <p class="text-lg text-justify mx-4 mt-4 tracking-tight">
             Our fundraising goal for this year is $300,000. So far, we are at 55% of our fundraising
             goal with contributions from 25.4% of parents. Imagine what we can do for Lynbrook with
             100% parent participation!
           </p>
         </div> 
-        <div class="flex flex-col gap-2 w-full items-center">
+        <div class="flex flex-col gap-2 w-full items-center px-4 sm:px-8">
             <progress class="progress progress-primary w-full" value="25" max="100"></progress>
             <p class="text-xl tracking-wider">25% parents have contributed</p>
             <progress class="progress progress-secondary w-full" value="55" max="100"></progress>
